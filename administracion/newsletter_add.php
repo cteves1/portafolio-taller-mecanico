904fd62f240d0a5ab782a3b164277628d423d1f9
Raklo98 <cristianteves1@gmail.com>
2017-10-27 08:21:19 -0300

    se envia el correo del newletter

--- administracion/newsletter_add.php
+++ administracion/newsletter_add.php
@@ -1,11 +1,29 @@
 <?php
 require '../php/conexion.php';
-print_r($_POST);
 if(isset($_POST['tittle']) && isset($_POST['message']) && isset($_POST['landing'])){
 
             $insert = mysqli_query($conexion, "INSERT INTO newsletters (tittle,message,id_ladingpage,created_at,exp_at) 
                                                               VALUES ('".$_POST['tittle']."','".$_POST['message']."','".$_POST['landing']."','".$_POST['created_at']."','".$_POST['exp_at']."')");
-            header("Location: newsletter.php?msg=8");
+            $enviar = mysqli_query($conexion, "SELECT email FROM usuarios WHERE sub = '1'");
+
+        while ($emails = mysqli_fetch_assoc($enviar)){
+            $para      = $emails['email'];
+            $titulo    = $_POST['tittle'];
+            $mensaje   = $_POST['message']." <a href='http://locahost/portafolio-taller-mecanico/landing-page.php?id='".$_POST['landing'].">Ver producto</a>";
+
+            $cabeceras = 'From: El_Paisano_Taller ' . "\r\n" .
+                'Reply-To: cristianteves1@gmail.com' . "\r\n" .
+                'X-Mailer: PHP/' . phpversion();
+            $cabeceras .= 'MIME-Version: 1.0' . "\r\n";
+            $cabeceras .= 'Content-Type: text/html; charset-iso-8859-1' . "\r\n";
+
+            mail($para, $titulo, $mensaje, $cabeceras);
+        }
+
+
+
+
+            //header("Location: newsletter.php?msg=8");
 }else{
     header("Location: index.php");
 }
