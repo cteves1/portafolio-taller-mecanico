92a21df58fd18245ed6ed91763cf2671803888ad
Raklo98 <cristianteves1@gmail.com>
2017-10-27 01:03:27 -0300

    se termino la seccion registros, este verifica si hay registros eliminados y si es asi muestra la cantidad de registros eliminados

--- php/conexion.php
+++ php/conexion.php
@@ -56,34 +56,19 @@ if(isset($_GET['msg'])){
 	$consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE id_usuario='{$_COOKIE['id_user']}'");
 }
 */
-	
-	//Eliminacion de registro
-	
-	$consulta_fecha = mysqli_query($conexion, "SELECT created_at, email FROM registros");
-
-	
-	while ($registro=mysqli_fetch_assoc($consulta_fecha)) {
-		$fecha=$registro['created_at'];
-		$email=$registro['email'];
-		//echo $mail;
-			//echo "Fecha before: ".$fecha;
-		//Tomamos la fecha de la base de datos
-			//$fecha=date("d-m-Y", strtotime($fecha));
-		//echo "Fecha after: ".$fecha;
-		$exp_date=strtotime ( '+1 day' , strtotime ( $fecha ) ) ;
-		//echo $exp_date;
-		//Convertimos la fecha de Y-m-d a d-m-Y.
-		//Creamos una variable para la fecha de expiración agregándole la cantidad de días que querramos
-		$exp_date= date ( 'd-m-Y' , $exp_date );
-		$hoy = date("d-m-Y");
-		//echo "<p>Fecha de entrada: ".$fecha."</p>";
-		//echo "<p>Exp date: ".$exp_date."</p>";
-		//echo "<p>Hoy: ".$hoy."</p>";
-		if (strtotime($hoy)>strtotime($exp_date)) {
-
-			$borrar=mysqli_query($conexion, "DELETE * FROM registros WHERE email='".$email."'");
-		}
-		//La función strtotime($fechax); lo que hace es pasar un formato de fecha inglés a fecha unix que es expesada en segundos-
-		// Al ser segundos los podemos comprar con un if y obtener resultado.
+$registros = mysqli_query($conexion, "SELECT * FROM registros");
+//Eliminacion de registro
+$deletes = 0;
+    while ($registro = mysqli_fetch_assoc($registros)) {
+        $exp = time() - strtotime($registro["created_at"]);
+        if( $exp > 3600){
+            $borrar= mysqli_query($conexion, "DELETE FROM registros WHERE email='".$registro["email"]."'");
+            $deletes++;
+        }
 	}
+	if ($deletes> 0){
+        $alert = false;
+        $alert_msg = "Se borraron ".$deletes." registro expirados";
+        $alert_type = "success";
+    }
  ?>
